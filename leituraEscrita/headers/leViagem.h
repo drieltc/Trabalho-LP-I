defffb08604e464ab38730e8ca6b9ea8544efbca
Adriel Costa <adryeltc2001@gmail.com>
2024-08-21 13:03:23 -0300

    Assinatura da Funcao carregarViagens

--- leituraEscrita/headers/leViagem.h
+++ leituraEscrita/headers/leViagem.h
@@ -9,6 +9,11 @@
 using namespace std;
 
 void salvarViagem(Viagem *viagem);
-vector<Viagem>* carregarViagens(vector<Cidade>* cidades, function<Cidade*(const string&)> pesquisarCidade);
+vector<Viagem>* carregarViagens(
+    vector<Cidade>* cidades, function<Cidade*(const string&)> pesquisarCidade,
+    vector<Passageiro>* passageiros, function<Passageiro*(const string&)> pesquisarPassageiro,
+    vector<Transporte>* transportes, function<Transporte*(const string&)> pesquisarTransporte,
+    vector<Trajeto>* trajetos, function<Trajeto*(const string&, const string&, const char&)> pesquisarTrajeto
+);
 
 #endif // LEViagem_H
